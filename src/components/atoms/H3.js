04e5b643e87fe99ceae22c71c0bc91300e391564
yafsw <yafswctrlz@gmail.com>
2021-03-08 11:09:10 +0700

    Add H3 component

---
+++ src/components/atoms/H3.js
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+
+const H3 = props => {
+    return (
+        <H3Style
+            fweight={props.fweight}
+            fsize={props.fsize}
+            color={props.color}
+            alig={props.align}
+        >   {props.children}
+        </H3Style>
+    );
+};
+
+const H3Style = Styled.h3`
+    font-weight: ${props => props.fweight ? props.fweight : 'bold'};
+    font-size: ${props => props.fsize ? props.fsize : '2.8rem'};
+    color: ${props => props.color ? props.color : 'black'};
+    text-align: ${props => props.align ? props.align : 'left'};
+`;  
+
+export default memo(H3);
