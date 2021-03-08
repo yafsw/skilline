78408091668184e180735c0d3425d0e7ccbfb6ce
yafsw <yafswctrlz@gmail.com>
2021-03-08 15:01:39 +0700

    Update Logo component

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -4,13 +4,16 @@ import Styled from 'styled-components';
 const Logo = () => {
     return (
         <LogoStyle>
-
+            Skilline
         </LogoStyle>
     );
 };
 
 const LogoStyle = Styled.span`
     font-size: 3.2rem;
+    color: black;
+    letter-spacing: 0.04rem;
+    font-weight: 700;
 `;
 
 export default memo(Logo);
