ed063fc02e889b35bce9588083c16f2acf044924
yafsw <yafswctrlz@gmail.com>
2021-03-08 14:39:14 +0700

    Add Home page

---
+++ src/page/Home.js
@@ -0,0 +1,12 @@
+import React, { memo } from 'react';
+import Header from '../components/organism/Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}:
+
+export default memo(Home);
