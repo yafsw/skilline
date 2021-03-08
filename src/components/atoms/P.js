61e87747dc3e43833d61b7c200d66b7559e7c322
yafsw <yafswctrlz@gmail.com>
2021-03-08 11:03:52 +0700

    Add P component

---
+++ src/components/atoms/P.js
@@ -0,0 +1,23 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+
+const P = props => {
+    return (
+        <PStyle
+            fweight={props.fweight}
+            fsize={props.fsize}
+            color={props.color}
+            alig={props.align}
+        >   {props.children}
+        </PStyle>
+    );
+};
+
+const PStyle = Styled.p`
+    font-weight: ${props => props.fweight ? props.fweight : 'normal'};
+    font-size: ${props => props.fsize ? props.fsize : '2.4rem'};
+    color: ${props => props.color ? props.color : 'black'};
+    text-align: ${props => props.align ? props.align : 'left'};
+`;
+
+export default memo(P);
