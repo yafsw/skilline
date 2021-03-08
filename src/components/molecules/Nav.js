da7c4ad0e65a7a05313acfe2f15b2761be9299eb
yafsw <yafswctrlz@gmail.com>
2021-03-08 15:45:03 +0700

    Update Nav component

--- src/components/molecules/Nav.js
+++ src/components/molecules/Nav.js
@@ -12,14 +12,27 @@ const Nav = () => {
             </div>
             <div>
                 <div>
-                    <Span>Home</Span>
-                    <Span>Careers</Span>
-                    <Span>Blog</Span>
-                    <Span>About Us</Span>
+                    <Span fsize="2.2rem">Home</Span>
+                    <Span fsize="2.2rem">Careers</Span>
+                    <Span fsize="2.2rem">Blog</Span>
+                    <Span fsize="2.2rem">About Us</Span>
                 </div>
                 <div>
-                    <Button>Login</Button>
-                    <Button>Sign Up</Button>
+                    <Button
+                        fsize="2.2rem"
+                        fweight="500"
+                        color="#6C6C6C"
+                        padding="1.4rem 4.8rem"
+                    >   Login
+                    </Button>
+                    <Button
+                        fsize="2.2rem"
+                        fweight="500"
+                        color="#FFFFFF"
+                        bgcolor="#F48C06"
+                        padding="1.4rem 3.6rem"
+                    >   Sign Up
+                    </Button>
                 </div>
             </div>
         </NavStyle>
@@ -31,7 +44,39 @@ const NavStyle = Styled.nav`
     justify-content: space-between;
     align-items: center;
     width: 100%;
-    max-width: 144rem;
+    max-width: 164rem;
+
+    div:nth-child(2) {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+
+        div {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            @media(max-width: 64rem) {
+                display: none;
+            }
+
+            span {
+                margin-right: 7.2rem;
+
+                @media(max-width: 84rem) {
+                    margin-right: 4.8rem;
+                }
+            }   
+
+            button:first-child {
+                margin-right: 3.2rem;
+
+                @media(max-width: 84rem) {
+                    margin-right: 2.4rem;
+                }
+            }
+        }
+    }
 `;
 
 export default memo(Nav);
