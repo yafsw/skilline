94641a4c4ccd23b4aed4e27fdde1676e93652493
yafsw <yafswctrlz@gmail.com>
2021-03-09 16:56:03 +0700

    Add context

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,13 @@
 import React, { memo, StrictMode } from 'react';
+import { AppProvider } from '../cotext';
 import Home from '../page/Home';
 
 const App = () => {
     return (
         <StrictMode>
-            <Home/>
+            <AppProvider>
+                <Home/>
+            </AppProvider>
         </StrictMode>
     );
 };
---
+++ src/cotext/index.js
@@ -0,0 +1,21 @@
+import React, { createContext, useCallback, useReducer } from 'react';
+import reducer from './reducer';
+import initialState from './state';
+
+const AppContext = createContext();
+
+export const useApp = () => useApp(AppContext);
+
+export const AppProvider = props => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+
+    const handleBar = useCallback(() => dispatch({ type: 'HANDLE_BAR' }), []);
+
+    const action = { handleBar };
+
+    return (
+        <AppContext.Provider value={{state, action}}>
+            {props.children}
+        </AppContext.Provider>
+    );
+};
