02a8886038412adb504f08551c6d95dd266da8e0
yafsw <yafswctrlz@gmail.com>
2021-03-09 21:52:41 +0700

    Update Bar component style

--- src/components/molecules/Bar.js
+++ src/components/molecules/Bar.js
@@ -2,10 +2,13 @@ import React, { memo } from 'react';
 import Styled from 'styled-components';
 import Button from '../atoms/Button';
 import Span from '../atoms/Span';
+import { useApp } from '../../cotext';
 
 const Bar = () => {
+    const { state } = useApp();
+
     return (
-        <BarStyle>
+        <BarStyle bar={state.bar}>
             <div>
                 <Span fsize="2.2rem">Home</Span>
                 <Span fsize="2.2rem">Careers</Span>
@@ -34,7 +37,34 @@ const Bar = () => {
 };
 
 const BarStyle = Styled.div`
+    display: ${props => props.bar ? 'flex' : 'none'};
+    flex-direction: column;
+    justify-content: center;
+    align-items: flex-start;
+    width: 100%;
+    padding: 3.6rem;
+    transition: .3s ease-in-out;
 
+    @media(min-width: 68rem) {
+        display: none;
+    }
+
+    div {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: flex-start;
+        width: 100%;
+
+        span {
+            margin-bottom: 1.2rem;
+        }
+
+        button {
+            width: 100%;
+            margin-top: 1.2rem;
+        }
+    }
 `;
 
 export default memo(Bar);
