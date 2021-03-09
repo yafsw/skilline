4dbc7a1103e171324241b258fa53a39f9cc54c25
yafsw <yafswctrlz@gmail.com>
2021-03-09 15:43:21 +0700

    Update Logo component style

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -4,16 +4,35 @@ import Styled from 'styled-components';
 const Logo = () => {
     return (
         <LogoStyle>
-            Skilline
+            <div></div>
+            <span>Skilline</span>
         </LogoStyle>
     );
 };
 
-const LogoStyle = Styled.span`
-    font-size: 3.2rem;
-    color: black;
-    letter-spacing: 0.04rem;
-    font-weight: 700;
+const LogoStyle = Styled.div`
+    position: relative;
+
+    span {
+        font-size: 3.2rem;
+        color: black;
+        letter-spacing: 0.04rem;
+        font-weight: 700;
+        position: absolute;
+        top: 50%;
+        line-height: 0;
+        left: 50%;
+    }
+
+    div {
+        width: 5.6rem;
+        height: 5.6rem;
+        border-radius: .6rem;
+        background-color: #65DAFF;
+        transform: rotate(45deg);
+        position: relative;
+        left: 1.6rem;
+    }
 `;
 
 export default memo(Logo);
