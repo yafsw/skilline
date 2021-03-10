2391cdcbc0d7ed86a3b88b80aa09ea20e1f5dd48
yafsw <yafswctrlz@gmail.com>
2021-03-10 10:58:32 +0700

    Update Home page

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,10 +1,14 @@
 import React, { memo } from 'react';
 import Header from '../components/organism/Header';
+import Heading from '../components/organism/Heading';
 
 const Home = () => {
     return (
         <>
             <Header />
+            <main>
+                <Heading />
+            </main>
         </>
     );
 };
