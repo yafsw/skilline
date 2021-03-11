2f73067406238b2398f3deef880f48a57504d4a1
yafsw <yafswctrlz@gmail.com>
2021-03-11 20:25:58 +0700

    Add AIO component

---
+++ src/components/organism/AIO.js
@@ -0,0 +1,32 @@
+import React, { memo } from 'react';
+import Styled from 'styled-components';
+import H2 from '../atoms/H2';
+import P from '../atoms/P';
+
+const AIO = () => {
+    return (
+        <AIOStyle>
+            <H2 color="#2F327D" align="center">All-In-One <span className="title">Cloud Software.</span></H2>
+            <P color="#696984" align="center">
+                Skilline is one powerful online software suite that combines all the tools needed to run a successful school or office.
+            </P>
+        </AIOStyle>
+    );
+};
+
+const AIOStyle = Styled.section`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    padding: 4.8rem;
+
+    .title {
+        font-weight: bold;
+        font-size: 5.4rem;
+        color: #F48C06;
+    }
+`;
+
+export default memo(AIO);
