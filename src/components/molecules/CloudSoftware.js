c06c600cc31708470a83290d3ec4a9a6b11fe30b
yafsw <yafswctrlz@gmail.com>
2021-03-13 14:37:34 +0700

    Update CloudSoftware compoent

--- src/components/molecules/CloudSoftware.js
+++ src/components/molecules/CloudSoftware.js
@@ -6,8 +6,11 @@ import P from '../atoms/P';
 const CloudSoftware = props => {
     return (
         <CloudSoftwareStyle>
-            <H3 fsize="3rem" color="#2F327D" fweight="500">{props.h3}</H3>
-            <P fsize="2rem" color="#696984">{props.p}</P>
+            <div bgimg={props.bgimg}>
+                <img src={props.img} alt={props.img} />
+            </div>
+            <H3 fsize="3rem" color="#2F327D" fweight="500" align="center">{props.h3}</H3>
+            <P fsize="2rem" color="#696984" align="center">{props.p}</P>
         </CloudSoftwareStyle>
     );
 };
@@ -19,7 +22,25 @@ const CloudSoftwareStyle = Styled.div`
     height: 43rem;
     border-radius: 2rem;
     box-shadow: 0px 10px 60px rgba(38, 45, 118, 0.08);
-    padding: 4.8rem;
+    padding: 14.8rem 4.8rem 4.8rem;
+    position: relative;
+
+    div {
+        display: flex;
+        flex-direction: center;
+        align-items: center;
+        postition: absolute;
+        background: ${props => props.bgimg ? props.bgimg : 'black'}:
+        width: 10rem;
+        height: 10rem;
+        box-shadow: 0px 10px 40px rgba(54, 61, 136, 0.06);
+        padding: 1.2rem;
+        border-radius: 10rem;
+
+        &:hover {
+            box-shadow: 0px 16px 40px rgba(37, 44, 113, 0.1);
+        }
+    }
 `;
 
 export default memo(CloudSoftware);
